22fe26adaaf99048d4159e7f93852e5b59b90a15
leonMarkelov111 <85875697+leonMarkelov111@users.noreply.github.com>
2022-05-23 21:49:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import vk_api
+import vk_api, json
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import datetime
 import re
@@ -16,12 +17,9 @@ def kikuser(id, user):
     vk_session.method("messages.removeChatUser", {"chat_id": id, "user_id": user, "random_id": 0})
 
 
-
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
         if event.from_chat:
-
-
             id = event.chat_id
             msg = event.object.message['text']
             cmd = msg.split(' ')[0].replace('/', '')
@@ -30,15 +28,34 @@ for event in longpoll.listen():
             if cmd in cmds and '[' in msg and ']' in msg:
                 try:
                     idd = re.findall(rf'/{cmd} \[id(\d*)\|.*]', msg)[0]
-                    reason = msg.split(' ')[2]
                     kick_type = 'С возможностью возвращения'
-                    kikuser(id, idd)
-                    sender(id, f"@id{idd}(Пользователь) успешно исключён из беседы.\nАдминистратор: @id{iduser}(Администратор)\nДата:  {datetime.datetime.now().strftime(' %d.%m.%Y')}\nВремя:{datetime.datetime.now().strftime(' %H:%M:%S')}\nПричина: {reason}\nТип кика: {kick_type}")
-                    print(iduser, 'use command kick', idd)
-                except:
-                    sender(id, f'@id{iduser}(Администратор), за это тебя могут снять!\n\nВозможные причины появления данного сообщения:\n1. Вы попытались кикнуть человека старше Вас рангом.\n2. Вы не указали причину для кика\n3. Вы гей')
-                    print('Администратор с id',iduser, ' Попытка кикнуть одного из членов whitelist')
+                    try:
+                        msg1 = msg.split(' ')[2]
+                        kikuser(id, idd)
+                        sender(id,
+                               f"@id{idd}(Пользователь) успешно исключён из беседы.\nАдминистратор: @id{iduser}(Администратор)\n"
+                               f"Дата:  {datetime.datetime.now().strftime(' %d.%m.%Y')}\nВремя:{datetime.datetime.now().strftime(' %H:%M:%S')}\n"
+                               f"Причина: {msg1}\nТип кика: {kick_type}")
+                        print(iduser, 'use command kick', idd)
+                    except:
+                        kikuser(id, idd)
+                        sender(id,
+                               f"@id{idd}(Пользователь) успешно исключён из беседы.\nАдминистратор: @id{iduser}(Администратор)\n"
+                               f"Дата:  {datetime.datetime.now().strftime(' %d.%m.%Y')}\nВремя:{datetime.datetime.now().strftime(' %H:%M:%S')}\n"
+                               f"Причина: Нет причины\nТип кика: {kick_type}")
+                        print(iduser, 'use command kick', idd)
+                except Exception as e:
+                    sender(id,
+                   f'@id{iduser}(Администратор), за это тебя могут снять!\n\nВозможные причины появления данного сообщения:'
+                   f'\n1. Вы попытались кикнуть человека старше Вас рангом.\n'
+                   f'2. Вы не указали причину для кика\n'
+                   f'3. Вы гей')
+            print('Администратор с id', iduser, ' Попытка кикнуть одного из членов whitelist')
             if msg in ['/time']:
                 sender(id, f"Текущее время и дата. \n\nНа данный момент в Москве: {datetime.datetime.now().strftime(' %H:%M:%S')}\nДата: {datetime.datetime.now().strftime(' %d.%m.%Y')}")
             if msg in ['/kick', '/кик']:
                 sender(id, 'Ошибка при использовании команды!\nИспользуйте /kick пользователь причина \n\nУчтите, что все Ваши действия логгируются!\n\nВ случае, если причина не будет указана Вас могут снять с поста!')
+            if msg == 'test':
+                sender(id, f"{datetime.datetime.now().strftime('%H:%M:%S')}")
+            if msg in ['/help', '/хелп', '/помощь', 'help', 'хелп', 'помощь']:
+                sender(id, f'Команды доступные основателю:\n\n/setowner - выдать права основателя в конференции\n/setspecadmin - выдать права специального администратора конференции\n/hallotext - установка приветственного сообщения\n/setting - установить различные настройки конфереции\n/checkpublick - проверить подписку на указанную группу\n\nКоманды специального администратора:\n\n/rkick - исключить людей, которых инвайтнули менее 24 часов назад\n/setadmin - добавить администратора\n/addfilter - добавить слово в фильтер\n/addmentionlist - добавить запрет на упоминание человека\n\nКоманды администратора:\n\n/setmoderation - добавить модератора\n/removeroles - снять привелегии\n/setnick - установить имя игроку\n/nicklist - просмотреть список ников\n/removenick - снять ник игрока\n/getnick - просмотреть ник\n/getnicksuser - получить вконтакте игрока через ник\n\nКоманды модератора:\n\n/mute - выставить затычку\n/unmute - снять затычку\n/mutelist - список замученых игроков\n/warn - выдать предупреждение\n/unwarn - снять предупреждение\n/warnlist - список пользователей с предупреждением\n/kick - исключить пользователя\n/ban - заблокировать пользователя\n/unban - разблокировать пользователя\n/banlist - список заблокированных пользователей\n/getmute - просмотреть информацию о муте\n/getwarn - посмотреть информацию о варне\n/checkpunish - посмотреть наказания\n/help - список команд\n/reg - просмотреть дату регистрации аккаунта\n/cc - очистить чат\n/online - просмотреть пользователей онлайн\n/stats - просмотреть статистику человека ')
